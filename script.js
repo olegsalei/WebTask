dc7b9324a468ce1bdc98ead993395a5b44871b43
Oleg Salei <olegsalei@mail.ru>
2016-10-30 19:22:45 +0300

    Getting data with XHR

--- script.js
+++ script.js
@@ -1,18 +1,62 @@
-(function() {
-    $.ajax({
-        url: 'https://api.discogs.com/database/search?q=Nirvana&key=gkBgtDWOpHTsEJulHJja&secret=HDHRcjWlagqyjsGamTTPJSNlnUgKUkyV ',
-        type: 'GET',
-        dataType: 'jsonp',
-        crossDomain: true,
-        success: function(data){
-            console.log(data.data);
-          
-      },
-      error: function(err){
-        console.log(err);
+'use strict';
+
+$(document).ready(function(){
+    
+/*
+    timer = setTimeout(function run(){
+      $(".loaded-info").remove();
+      addNewDiv();
+      repoSearch(language, repository);
+      timer = setTimeout(run, 30000);
+    }, 30000);
+  });
+  */
+
+  $("#search-form").on('submit', function(event){
+    event.preventDefault();
+    alert("Good");
+    var author = $("#author").val();
+    var title = $("#title").val();
+
+    alert(author + "  " + title);
+    search(title, author);  
+  });
+
+  function stopTimer(){
+    if(timer){
+      clearTimeout(timer);
+      timer = null;
+    }
+  };
+
+  
+});
+
+function search(title, author){
+    alert("In function");
+    var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.discogs.com/database/search?q=' + title + '&author=' + author + '&key=gkBgtDWOpHTsEJulHJja&secret=HDHRcjWlagqyjsGamTTPJSNlnUgKUkyV', true);
+    xhr.onload = function(){
+      console.log("Goos");
+      alert(this.status);
+      /*if(this.status == 422){
+        console.log("422 Unprocessable Entity");
       }
-    });
+      if(this.status == 400){
+        console.log("400 Bad Request");
+      }*/
+      if(this.status == 200){
+        var responseData = JSON.parse(xhr.responseText);
+        console.log(responseData);
 
-
-
-})();
+        //var source = $("#repos-entry-template").html();
+        //var template = Handlebars.compile(source);
+        //$('.loaded-info').append(template({objects:responseData.items}));
+      }
+    }
+    xhr.onerror = function(){
+      console.log(this.status);
+    }
+    xhr.send();
+  };
