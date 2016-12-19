5e10e03dba504918d63d43eea342ab1a1bcae6d8
Oleg Salei <olegsalei@mail.ru>
2016-12-19 09:55:47 +0300

    Delete server js, add normal error processing

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function addNewDiv(){
 function search(title){
     var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://api.discogs.com/database/search?q=' + title + '&sort_order=desc', true);
+    xhr.open('GET', 'https://api.discogs.com/database/search?q=' + title + '&sort_order=desc&key=gkBgtDWOpHTsEJulHJja&secret=HDHRcjWlagqyjsGamTTPJSNlnUgKUkyV', true);
     xhr.onreadystatechange = function(onEvent) {
       if(xhr.readyState === 4){
         if(xhr.status === 200){
@@ -51,61 +51,22 @@ function search(title){
           }
         }
       } else{
-        if(xhr.status == 500){
-          console.log("500 Internal Server Error");
+        if(xhr.status >= 500){
+          console.log("Server Error");
         }
-        if(xhr.status == 422){
-          console.log("422 Unprocessable Entity");
+        if(xhr.status <500 && xhr.status >=400){
+          console.log("Client Error ");
         }
-        if(xhr.status == 405){
-          console.log("405 Method Not Allowed");
+        if(xhr.status <400 && xhr.status >=300){
+          console.log("Redirection");
         }
-        if(xhr.status == 404){
-          console.log("404 Not Found");
+        if(xhr.status <300 && xhr.status >200){
+          console.log("Success");
         }
-        if(xhr.status == 403){
-          console.log("403 Forbidden");
-        }
-        if(xhr.status == 401){
-          console.log("401 Unauthorized");
-        }
-        if(xhr.status == 204){
-          console.log("204 No Content");
-        }
-        if(xhr.status == 201){
-          console.log("201 Continue");
+        if(xhr.status <200 && xhr.status >=100){
+          console.log("Informational");  
         }
       }
     }
-    // xhr.onload = function(){
-    //   if(this.status == 200){
-    //     var responseData = JSON.parse(xhr.responseText);
-    //     console.log(responseData);
-
-    //     var temp = [];
-    //     if (responseData.results.length !== 0) {
-    //         for (var i = 0; i < responseData.results.length; ++i) {
-    //             if(Object.keys(responseData.results[i]).length > 7){
-    //                 temp.push(responseData.results[i]);
-    //             }
-    //         }
-    //         var newObject = { temp };
-    //         var source   = $("#entry-template").html();
-    //         var template = Handlebars.compile(source);
-    //         $('.loaded').append(template({objects:newObject.temp}));
-    //     } else {
-    //         var newDiv = document.createElement("div");
-    //         newDiv.className = "panel-danger text-center";
-    //         newDiv.innerHTML = "<span class='text-center'>Sorry, we haven't information corresponding your request :(</span>";
-            
-    //         var beforeDiv = document.getElementById("header");
-    //         document.body.insertBefore(newDiv, beforeDiv);
-    //     }
-        
-    //   }
-    // }
-    /*xhr.onerror = function(){
-      console.log(this.status);
-    }*/
     xhr.send();
-};
+}
